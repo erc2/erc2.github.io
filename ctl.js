c811040815c3cb7c07a8ae1e6f33173a85052aba
unknown <bit4in@gmail.com>
2020-01-20 01:44:16 +0900

    .

--- ctl.js
+++ ctl.js
@@ -1,5 +1,13 @@
+/*addEventListener("DOMContentLoaded",function(){
+	var s=document.createElement("style");
+	document.head.appendChild(s);
+});
+function ctl(a){
+
+}
+*/
 function ctl(){
-	var lng=window["ctl:lang"],a1=document.head,t1=a1.querySelector("title"),lnk=ctl_list_link(),nod=ctl_list_node();
+	var lng=window["ctl:lang"],a1=document.head,t1=a1.querySelector("title"),lnk=link(),nod=node();
 
 	for(var a,i=0,c=nod.length;i<c;i++){
 		if(!(nod[i][0]+".css" in lnk)){
@@ -40,6 +48,36 @@ function ctl(){
 		if(c)a.onerror=c;
 		a1.insertBefore(a,t1);
 	}
+
+	function link(){
+		var r={};
+
+		for(var l=document.querySelectorAll("[title=ctl]"),i=l.length-1;i>=0;i--){
+			r[l[i].getAttribute("href")]=l[i];
+		}
+
+		return r;
+	}
+	function node(){
+		var a,m,n,l=document.querySelectorAll(".ctl"),r=new Array(l.length);
+
+		for(var i=l.length-1;i>=0;i--){
+			a=l[i].getAttribute("data-ctlu");
+			if(!a){
+				m=/ctl\s*(.*)/.exec(l[i].className);
+				if(!m[1])continue;
+				a=m[1].replace(/\s/g,"");
+			}
+			m=/((?:\s|^)\-[\-\w]+)/.exec(l[i].className);
+			if(n=l[i].getAttribute("data-ctlreg")){
+				n=n.split("\t");
+				a=a.replace(new RegExp(n[0]),n[1]);
+			}
+			r[i]=[a,m?m[1]:""];
+		}
+
+		return r;
+	}
 }
 function ctlcls(p){
 	for(var c=p.querySelectorAll(".ctl"),i=c.length-1;i>=0;i--)if(c[i].onblur)c[i].onblur();
@@ -53,48 +91,20 @@ function ctljs(n,proc){
 	}
 }
 function ctl_lang(pref){
-	var a,lang_text;
+	var a,text;
 	if("localStorage" in window&&"ctl:lang" in localStorage){
-		window["ctl:lang_text"]=lang_text=localStorage["ctl:lang_text"];
+		window["ctl:lang_text"]=text=localStorage["ctl:lang_text"];
 		window["ctl:lang"]=localStorage["ctl:lang"];
 	}else{
 		for(var m,q,l=navigator.languages,i=0,c=l.length;i<c;i++){
 			q=new RegExp("([^\\t]*)\\t?("+(l[i].indexOf("-")<0?l[i]+"-\\w+":l[i])+")","i");
 			if(m=pref.match(q)){
-				window["ctl:lang_text"]=lang_text=m[1];
+				window["ctl:lang_text"]=text=m[1];
 				window["ctl:lang"]=m[2];
 				if("localStorage" in window)localStorage["ctl:lang_text"]=m[1],localStorage["ctl:lang"]=m[2];
 				break;
 			}
 		}
 	}
-	if(a=document.querySelector(".lang .text"))a.textContent=lang_text;
-}
-function ctl_list_link(){
-	var r={};
-
-	for(var l=document.querySelectorAll("[title=ctl]"),i=l.length-1;i>=0;i--){
-		r[l[i].getAttribute("href")]=l[i];
-	}
-
-	return r;
-}
-function ctl_list_node(){
-	var l=document.querySelectorAll(".ctl"),r=new Array(l.length);
-
-	for(var a,m,n,i=l.length-1;i>=0;i--){
-		a=l[i].getAttribute("data-ctl-u");
-		if(!a){
-			m=/ctl\s*(.*)/.exec(l[i].className);
-			a=m[1].replace(/\s/g,"");
-		}
-		m=/((?:\s|^)\-[\-\w]+)/.exec(l[i].className);
-		if(n=l[i].getAttribute("data-ctl-ug")){
-			n=n.split(",");
-			a=a.replace(new RegExp(n[0]),n[1]);
-		}
-		r[i]=[a,m?m[1]:""];
-	}
-
-	return r;
+	if(a=document.querySelector(".lang .text"))a.textContent=text;
 }
